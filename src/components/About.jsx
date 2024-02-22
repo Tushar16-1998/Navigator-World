3236a7cf01a0c0d4012febe2ee3d556e31d2e48b
Tushar16-1998 <tusharsunder2@gmail.com>
2024-02-22 15:57:11 +0000

    about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,69 +1,43 @@
-// import React from 'react';
-// import {Link} from 'react-router-dom'
-// import Navbar from './NavBar';
-
-// function About () {
-//   return (
-//     <div>
-//         <Navbar />
-  
-//       <h1>About Us</h1>
-//       <p>Welcome to our awesome Application! This is the About page where you can learn more about us.</p>
-      
-//       <h3>Our Team</h3>
-//       <ul>
-//         <li><strong>
-//              <a href='https://github.com/Tushar16-1998' >Tushar Sunder</a>
-//              </strong></li>
-//         <li><strong>
-//         <a href='https://github.com/MateusCTO' >Mateus Lima</a>
-//             </strong></li>
-//       </ul>
-//       <Link to='/countries'>Back</Link>
-//     </div>
-//   );
-// };
-
-// export default About;
-
-
-
-///////////////////////////////////////////////////
-
-
-// About.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Heading, Text, List, ListItem } from '@chakra-ui/react';
+import { Box, Heading, Text, List, ListItem, Link as ChakraLink } from '@chakra-ui/react';
 import Navbar from './NavBar';
 
 function About() {
   return (
-    <Box p={8} maxW="600px" mx="auto">
+    <Box p={8} maxW="800px" mx="auto" borderRadius="xl"  boxShadow="dark-lg" border="1px solid #E2E8F0" shadow="dark-lg">
       <Navbar />
 
-      <Heading as="h1" fontSize="4xl" mb={4}>
+      <Heading as="h1" fontSize="4xl" mb={4} textAlign="center" color="teal.500">
         About Us
       </Heading>
-      <Text fontSize="lg">
-        Welcome to our awesome Application! This is the About page where you can learn more about us.
+      <Text fontSize="lg" textAlign="center" mb={6} >
+        <strong>Welcome to our extraordinary Application! Explore our story and get to know us better.</strong>
       </Text>
 
-      <Heading as="h3" fontSize="2xl" mt={8} mb={4}>
-        Our Team
+      <Heading as="h3" fontSize="2xl" mb={4} textAlign="center" color="teal.500">
+        Meet Our Team
       </Heading>
-      <List styleType="none" pl={0}>
-        <ListItem mb={4}>
-            <Link to='/tushar'>
-                <strong>Tushar Sunder</strong>
-            </Link>
+      <List pl={0}>
+        <ListItem mb={4} textAlign="center">
+          <ChakraLink 
+          as={Link} 
+          to='/tushar' 
+          color="teal.500" 
+          fontWeight="bold" 
+          _hover={{ textDecor: 'underline', color: 'teal.600' }}>
+            Tushar Sunder
+          </ChakraLink>
         </ListItem>
-        <ListItem>
-            <Link to='/mateus'><strong>Mateus Lima</strong></Link>
+        <ListItem textAlign="center">
+          <ChakraLink as={Link} to='/mateus' color="teal.500" fontWeight="bold" _hover={{ textDecor: 'underline', color: 'teal.600' }}>
+            Mateus Lima
+          </ChakraLink>
         </ListItem>
       </List>
-      <Link to='/countries' display="block" mt={6} color="teal.500" fontSize="lg" fontWeight="bold">
-        Back
+
+      <Link to='/countries' display="block" mt={8} color="teal.500" fontSize="lg" fontWeight="bold" textAlign="center" _hover={{ textDecor: 'underline', color: 'teal.600' }}>
+        Back to Countries
       </Link>
     </Box>
   );
