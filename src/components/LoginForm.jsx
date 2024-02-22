9bda3ac9a84bad9e4523c14f450e945265fc2c96
MateusCTO <farias.mateus@gmail.com>
2024-02-22 15:46:15 +0000

    homepage

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Button } from "@chakra-ui/react";
-import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, Flex } from "@chakra-ui/react";
 
 const LoginForm = ({
   setUsername,
@@ -9,38 +9,43 @@ const LoginForm = ({
   handleGuestAccess,
 }) => {
   return (
-    <Card>
-      <CardHeader>Login</CardHeader>
+    <Card border="1px solid">
+      <CardHeader fontWeight="bold">Login</CardHeader>
       <CardBody>
-        <form>
-          <label>
-            Username:
-            <input type="text" onChange={(e) => setUsername(e.target.value)} />
-          </label>
-          <br />
-          <label>
-            Password:
+        <form style={{ display: "flex", flexDirection: "column" }}>
+          <label style={{ margin: "5px" }}>
+            <strong>Username: </strong>
             <input
+              style={{ border: "1px solid", borderRadius: "10px" }}
+              type="text"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label style={{ margin: "5px" }}>
+            <strong>Password: </strong>
+            <input
+              style={{ border: "1px solid", borderRadius: "10px" }}
               type="password"
               onChange={(e) => setPassword(e.target.value)}
             />
           </label>
-          <br />
-          <Link to={"/countries"}>
-            <Button variant="solid" colorScheme="green" onClick={handleLogin}>
-              Login
-            </Button>
-          </Link>
+          <Flex justifyContent="space-around" m={"10px"}>
+            <Link to={"/countries"}>
+              <Button variant="solid" colorScheme="green" onClick={handleLogin}>
+                Login
+              </Button>
+            </Link>
 
-          <Link to={"/countries"}>
-            <Button
-              variant="solid"
-              colorScheme="green"
-              onClick={handleGuestAccess}
-            >
-              Guest Access
-            </Button>
-          </Link>
+            <Link to={"/countries"}>
+              <Button
+                variant="solid"
+                colorScheme="green"
+                onClick={handleGuestAccess}
+              >
+                Guest Access
+              </Button>
+            </Link>
+          </Flex>
         </form>
       </CardBody>
     </Card>
